58c67c9ddd3600c3ae2d9f96bcdea004016815d2
sreejith.menon <sreejith.menon@tatacommunications.com>
2019-03-21 15:39:44 +0530

    added-files

---
+++ src/app/forms/renderer/renderer.component.ts
@@ -0,0 +1,46 @@
+import { Component, AfterViewInit } from '@angular/core';
+import { PrismService } from '../../Prism.service';
+
+@Component({
+  selector: 'app-renderer',
+  templateUrl: './renderer.component.html',
+  styleUrls: ['./renderer.component.scss']
+})
+export class RendererComponent implements AfterViewInit {
+
+  form:any;
+  error:any;
+  storedForms:any[]=[""]
+  
+
+  constructor(public prism: PrismService) { }
+  ngAfterViewInit() {
+    this.prism.init();
+    this.getStoredJson();
+  }
+
+  getForm(formName){
+    this.form=JSON.parse(localStorage.getItem(formName));
+  }
+
+  buildForm(jsonString){
+    this.error=null;
+    try {
+      this.form=JSON.parse(jsonString);
+  }
+ catch(e) {
+      this.error=e
+  }
+    
+  }
+
+  getStoredJson(){
+    for(var i=0, len=localStorage.length; i<len; i++) {
+      var key = localStorage.key(i);
+      if (key.endsWith(".json")){
+      this.storedForms[i]=key}
+  }
+  }
+  
+
+}
